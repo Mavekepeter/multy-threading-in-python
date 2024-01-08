308ffa849696d1f45a36866e9df0039505503685
Mavekepeter <145150089+Mavekepeter@users.noreply.github.com>
2024-01-08 13:44:30 +0300

    Add files via upload

---
+++ condition thread.py
@@ -0,0 +1,30 @@
+from threading import *
+import time
+import random
+
+numbers=[]
+def taskA(c):
+   while True:
+      c.acquire()
+      num=random.randint(1,10)
+      print("Generated random number:", num)
+      numbers.append(num)
+      print("Notification issued")
+      c.notify()
+      c.release()
+      time.sleep(5)
+
+def taskB(c):
+   while True:
+      c.acquire()
+      print("waiting for update")
+      c.wait()
+      print("Obtained random number", numbers.pop())
+      c.release()
+      time.sleep(5)
+
+c=Condition()
+t1=Thread(target=taskB, args=(c,))
+t2=Thread(target=taskA, args=(c,))
+t1.start()
+t2.start()
